16c4f518bf55361c5cd8c204fed6c05db79b9171
Lenar Khafizov <lenarkhaf@gmail.com>
2025-07-30 20:37:14 +0300

    Выполнено домашнее задание к лекции 1 Основные понятия

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,10 +1,38 @@
 "use strict"
 function solveEquation(a, b, c) {
   let arr = [];
-  
+  let d = b ** 2 - 4 * a * c;
+  if (d > 0) {
+    arr[0] = (-b + Math.sqrt(d)) / (2 * a);
+    arr[1] = (-b - Math.sqrt(d)) / (2 * a);
+  } else if (d === 0) {
+    arr[0] = -b / (2 * a);
+  } else {
+    arr = [];
+  }
   return arr;
 }
 
 function calculateTotalMortgage(percent, contribution, amount, countMonths) {
-  
+  let rate = parseInt(percent) / 100 / 12; //процентная ставка
+  let initPay = parseInt(contribution); //первоначальный взнос 
+  let totalCost = parseInt(amount); //общая стоимость
+  let months = parseInt(countMonths); // срок кредита в месяцах
+
+  if (isNaN(rate) || rate < 0) {
+    return `Параметр "Процентная ставка" содержит неправильное значение "${percent}"`;
+  } else if (isNaN(initPay) || initPay < 0) {
+    return `Параметр "Первоначальный взнос" содержит неправильное значение "${contribution}"`;
+  } else if (isNaN(totalCost) || totalCost < 0) {
+    return `Параметр "Общая стоимость" содержит неправильное значение "${amount}"`;
+  } else {
+    if (isNaN(months) || months < 0) {
+      return `Параметр "срок ипотеки" содержит неправильное значение ${countMonths}`;
+    }
+    let body = totalCost - initPay; //тело кредита
+    let pay = body * (rate + rate / (((1 + rate) ** months) - 1)); //ежемесячная оплата
+    let totalAmount = (pay * months).toFixed(2);
+    console.log(totalAmount);
+    return +totalAmount;
+  }  
 }
