4672323c1c452764ea5bee128a6564151bad12ac
Lenar Khafizov <lenarkhaf@gmail.com>
2025-08-02 20:05:31 +0300

    Выполнено домашнее задание к лекции 3 Массивы

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,7 +1,15 @@
 function compareArrays(arr1, arr2) {
-  
+    if (arr1.length != arr2.length) {
+        return false;
+    }
+    return arr1.every((value, index) => {
+        return value === arr2[index];
+    });  
 }
 
 function getUsersNamesInAgeRange(users, gender) {
-  
+    const usersFiltered = Array.from(users.filter(user => user.gender === gender).map(user => user.age));
+    if (usersFiltered.length === 0) return 0;
+    const ageSum = usersFiltered.reduce((sum, age) => sum + age, 0);
+    return ageSum / usersFiltered.length;
 }
